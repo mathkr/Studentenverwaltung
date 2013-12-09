216f7f11c707eeeb6e0b71831f2745cffe5d9401
Matthis Krause <matthiskrause@hotmail.com>
2013-12-09 11:34:05 +0100

    implemented Student, added *.swo to .gitignore

--- src/studentenverwaltung/verwaltung/Student.java
+++ src/studentenverwaltung/verwaltung/Student.java
@@ -1,5 +1,64 @@
 package studentenverwaltung.verwaltung;
 
-public class Student {
+import studentenverwaltung.util.*;
 
+public class Student {
+	private static int nextMatrikelnummer = 10000;
+
+	private Name name;
+	private Date geburtstag;
+	private Studiengang studiengang;
+	private int bestandenePruefungen;
+	private int matrikelNummer;
+
+	public Student(Name name, Date geburtstag, Studiengang studiengang){
+		this.name = name;
+		this.geburtstag = geburtstag;
+		this.studiengang = studiengang;
+		this.bestandenePruefungen = 0;
+		this.matrikelNummer = nextMatrikelnummer++;
+	}
+
+	public void setPruefungBestanden(int index, boolean bestanden) throws IndexOutOfBoundsException {
+		if(index < 0 || index > 31)
+			throw new IndexOutOfBoundsException("Index: " + index + ", size: 31");
+
+		if(bestanden)
+			bestandenePruefungen |= 1 << index;
+		else
+			bestandenePruefungen &= ~(1 << index);
+	}
+
+	public boolean getPruefungBestanden(int index) throws IndexOutOfBoundsException {
+		if(index < 0 || index > 31)
+			throw new IndexOutOfBoundsException("Index: " + index + ", size: 31");
+		
+		return ((bestandenePruefungen >> index) & 1) == 1;
+	}
+
+	public int getAnzahlPruefungenBestanden(){
+		int res = 0;
+		for(int i = 0; i < 32; ++i){
+			if(getPruefungBestanden(i))
+				++res;
+		}
+
+		return res;
+	}
+
+	public Name getName(){
+		return name;
+	}
+
+	public Date getDate(){
+		return geburtstag;
+	}
+
+	public int getAge(){
+		return Date.getToday().getYearsSince(geburtstag);
+	}
+
+	public Studiengang getStudiengang(){
+		return studiengang;
+	}
 }
