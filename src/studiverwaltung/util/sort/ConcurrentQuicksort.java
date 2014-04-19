d4a869dd6f4dde82c95dde5f2d047b2dbb72638c
Matthis Krause <matthiskrause@hotmail.com>
2014-04-19 10:56:32 +0200

    Start to fix code Formatting

--- src/studiverwaltung/util/sort/ConcurrentQuicksort.java
+++ src/studiverwaltung/util/sort/ConcurrentQuicksort.java
@@ -13,10 +13,15 @@ public class ConcurrentQuicksort<T extends Comparable<T>> implements Runnable {
 
 	private IncrementLatch threadCountDown;
 
-	private static final int MAX_THREADS = Runtime.getRuntime().availableProcessors();
-	private static final ExecutorService THREADPOOL = Executors.newFixedThreadPool(MAX_THREADS);
+	private static final int MAX_THREADS =
+	    Runtime.getRuntime().availableProcessors();
 
-	private ConcurrentQuicksort(SimpleCollection<T> col, int left, int right, int minArraySize, IncrementLatch threadCountDown){
+	private static final ExecutorService THREADPOOL =
+	    Executors.newFixedThreadPool(MAX_THREADS);
+
+	private ConcurrentQuicksort(SimpleCollection<T> col, int left,
+	    int right, int minArraySize, IncrementLatch threadCountDown)
+	{
 		this.col = col;
 		this.left = left;
 		this.right = right;
@@ -24,26 +29,34 @@ public class ConcurrentQuicksort<T extends Comparable<T>> implements Runnable {
 		this.threadCountDown = threadCountDown;
 	}
 
-	public static <T extends Comparable<T>> void sort(SimpleCollection<T> col){
+	public static <T extends Comparable<T>> void sort(
+	    SimpleCollection<T> col)
+	{
 		shuffle(col);
 		swap(col, maxIndex(col), col.size() - 1);
 		beginSort(col);
 	}
 
-	private static <T extends Comparable<T>> int maxIndex(SimpleCollection<T> col){
+	private static <T extends Comparable<T>> int maxIndex(
+	    SimpleCollection<T> col)
+	{
 		int max = 0;
 
-		for(int i = 1; i < col.size(); ++i){
-			if(col.get(i).compareTo(col.get(max)) > 0) max = i;
+		for (int i = 1; i < col.size(); ++i){
+			if (col.get(i).compareTo(col.get(max)) > 0) max = i;
 		}
 
 		return max;
 	}
 
-	private static <T extends Comparable<T>> void beginSort(SimpleCollection<T> col){
+	private static <T extends Comparable<T>> void beginSort(
+	    SimpleCollection<T> col)
+	{
 		IncrementLatch allDone = new IncrementLatch(1);
 
-		new ConcurrentQuicksort<T>(col, 0, col.size() - 1, col.size() / MAX_THREADS, allDone).quicksort(0, col.size() - 1, true);
+		new ConcurrentQuicksort<T>(col, 0, col.size() - 1,
+		    col.size() / MAX_THREADS, allDone)
+		    .quicksort(0, col.size() - 1, true);
 
 		try {
 			allDone.await();
@@ -52,37 +65,38 @@ public class ConcurrentQuicksort<T extends Comparable<T>> implements Runnable {
 		}
 	}
 
-	private static <T> void shuffle(SimpleCollection<T> col){
-		// fisher & yates shuffling algorithm
-		for(int i = col.size() - 1; i > 0; --i){
+	private static <T> void shuffle(SimpleCollection<T> col) {
+		/* fisher & yates shuffling algorithm */
+		for (int i = col.size() - 1; i > 0; --i) {
 			int j = (int)(Math.random() * (i + 1));
 			swap(col, j, i);
 		}
 	}
 
 	@Override
-	public void run(){
+	public void run() {
 		quicksort(left, right, true);
 	}
 
-	private void quicksort(int left, int right, boolean initialCall){
-		// check for break condition
-		if(left >= right){
-			if(initialCall) threadCountDown.decrement();
+	private void quicksort(int left, int right, boolean initialCall) {
+		/* check for break condition */
+		if (left >= right) {
+			if (initialCall) threadCountDown.decrement();
 			return;
 		}
 
-		// find pivot and position it on the left
+		/* find pivot and position it on the left */
 		int mid = left + (right - left) / 2;
 		swap(col, left, median_of_three(col, left, mid, right));
 
-		// partition
+		/* partition */
 		mid = partition(col, left, left, right + 1);
 
-		// repeat recursively
-		if((mid - 1) - left >= minArraySize){
+		/* repeat recursively */
+		if ((mid - 1) - left >= minArraySize) {
 			threadCountDown.increment();
-			THREADPOOL.submit(new ConcurrentQuicksort<T>(col, left, mid - 1, minArraySize, threadCountDown));
+			THREADPOOL.submit(new ConcurrentQuicksort<T>(col, left,
+			    mid - 1, minArraySize, threadCountDown));
 
 			quicksort(mid + 1, right, false);
 		} else {
@@ -90,17 +104,19 @@ public class ConcurrentQuicksort<T extends Comparable<T>> implements Runnable {
 			quicksort(mid + 1, right, false);
 		}
 
-		if(initialCall) threadCountDown.decrement();
+		if (initialCall) threadCountDown.decrement();
 	}
 
-	private static <T extends Comparable<T>> int partition(SimpleCollection<T> col, int pivot, int left, int right){
+	private static <T extends Comparable<T>> int partition(
+	    SimpleCollection<T> col, int pivot, int left, int right)
+	{
 		T piv = col.get(pivot);
 
 		while(true){
 			while(col.get(++left ).compareTo(piv) < 0);
 			while(col.get(--right).compareTo(piv) > 0);
 
-			if(left >= right) break;
+			if (left >= right) break;
 			swap(col, left, right);
 		}
 
@@ -108,29 +124,31 @@ public class ConcurrentQuicksort<T extends Comparable<T>> implements Runnable {
 		return right;
 	}
 
-	private static <T extends Comparable<T>> int median_of_three(SimpleCollection<T> col, int aIndex, int bIndex, int cIndex){
+	private static <T extends Comparable<T>> int median_of_three(
+	    SimpleCollection<T> col, int aIndex, int bIndex, int cIndex)
+	{
 		T a = col.get(aIndex);
 		T b = col.get(bIndex);
 		T c = col.get(cIndex);
 
-		if(a.compareTo(b) > 0){
-			if(b.compareTo(c) > 0){
+		if (a.compareTo(b) > 0) {
+			if (b.compareTo(c) > 0) {
 				return bIndex;
-			} else if(a.compareTo(c) > 0){
+			} else if (a.compareTo(c) > 0) {
 				return cIndex;
 			} else {
 				return aIndex;
 			}
-		} else if(a.compareTo(c) > 0){
+		} else if (a.compareTo(c) > 0) {
 			return aIndex;
-		} else if(b.compareTo(c) > 0){
+		} else if (b.compareTo(c) > 0) {
 			return cIndex;
 		} else {
 			return bIndex;
 		}
 	}
 
-	private static <T> void swap(SimpleCollection<T> col, int a, int b){
+	private static <T> void swap(SimpleCollection<T> col, int a, int b) {
 		T tmp = col.get(a);
 		col.set(a, col.get(b));
 		col.set(b, tmp);
--- src/studiverwaltung/util/sort/Quicksort.java
+++ src/studiverwaltung/util/sort/Quicksort.java
@@ -2,61 +2,72 @@ package studiverwaltung.util.sort;
 
 import studiverwaltung.util.structures.SimpleCollection;
 
+/**
+ * hallo liebe Gemeinde!
+ */
 public class Quicksort {
-	public static <T extends Comparable<T>> void sort(SimpleCollection<T> col){
+	public static <T extends Comparable<T>> void sort(
+	    SimpleCollection<T> col)
+	{
 		shuffle(col);
 		swap(col, maxIndex(col), col.size() - 1);
 		quicksort(col, 0, col.size() - 1);
 	}
 
-	,rivate static <T extends Comparable<T>> int maxIndex(SimpleCollection<T> col){
+	private static <T extends Comparable<T>> int maxIndex(
+	    SimpleCollection<T> col)
+	{
 		int max = 0;
 
-		for(int i = 1; i < col.size(); ++i){
-			if(col.get(i).compareTo(col.get(max)) > 0) max = i;
+		for (int i = 1; i < col.size(); ++i) {
+			if (col.get(i).compareTo(col.get(max)) > 0) max = i;
 		}
 
 		return max;
 	}
 
-	private static <T> void shuffle(SimpleCollection<T> col){
-		// fisher & yates shuffling algorithm
-		for(int i = col.size() - 1; i > 0; --i){
+	private static <T> void shuffle(SimpleCollection<T> col) {
+		/* fisher & yates shuffling algorithm */
+		for (int i = col.size() - 1; i > 0; --i) {
 			int j = (int)(Math.random() * (i + 1));
 			swap(col, j, i);
 		}
 	}
 
-	private static <T extends Comparable<T>> void quicksort(SimpleCollection<T> col, int left, int right){
-		// check for break condition
-		if(left >= right) return;
+	private static <T extends Comparable<T>> void quicksort(
+	    SimpleCollection<T> col, int left, int right)
+	{
+		/* check for break condition */
+		if (left >= right) return;
 
-		// find pivot and position it on the left
+		/* find pivot and position it on the left */
 		int mid = left + (right - left) / 2;
 		swap(col, left, median_of_three(col, left, mid, right));
 
-		// partition
+		/* partition */
 		mid = partition(col, left, left, right + 1);
 
-		// repeat recursively
+		/* repeat recursively */
 		quicksort(col, left, mid - 1 );
 		quicksort(col, mid + 1, right);
 	}
 
-	private static <T> void swap(SimpleCollection<T> col, int a, int b){
+	private static <T> void swap(SimpleCollection<T> col, int a, int b) {
 		T tmp = col.get(a);
 		col.set(a, col.get(b));
 		col.set(b, tmp);
 	}
 
-	private static <T extends Comparable<T>> int partition(SimpleCollection<T> col, int pivot, int left, int right){
+	private static <T extends Comparable<T>> int partition(
+	    SimpleCollection<T> col, int pivot, int left, int right)
+	{
 		T piv = col.get(pivot);
 
-		while(true){
-			while(col.get(++left).compareTo(piv) < 0);
-			while(col.get(--right).compareTo(piv) > 0);
+		while (true) {
+			while (col.get(++left).compareTo(piv) < 0);
+			while (col.get(--right).compareTo(piv) > 0);
 
-			if(left >= right) break;
+			if (left >= right) break;
 			swap(col, left, right);
 		}
 
@@ -64,26 +75,27 @@ public class Quicksort {
 		return right;
 	}
 
-	private static <T extends Comparable<T>> int median_of_three(SimpleCollection<T> col, int aIndex, int bIndex, int cIndex){
+	private static <T extends Comparable<T>> int median_of_three(
+	    SimpleCollection<T> col, int aIndex, int bIndex, int cIndex)
+	{
 		T a = col.get(aIndex);
 		T b = col.get(bIndex);
 		T c = col.get(cIndex);
 
-		if(a.compareTo(b) > 0){
-			if(b.compareTo(c) > 0){
+		if (a.compareTo(b) > 0) {
+			if (b.compareTo(c) > 0) {
 				return bIndex;
-			} else if(a.compareTo(c) > 0){
+			} else if (a.compareTo(c) > 0) {
 				return cIndex;
 			} else {
 				return aIndex;
 			}
-		} else if(a.compareTo(c) > 0){
+		} else if (a.compareTo(c) > 0) {
 			return aIndex;
-		} else if(b.compareTo(c) > 0){
+		} else if (b.compareTo(c) > 0) {
 			return cIndex;
 		} else {
 			return bIndex;
 		}
 	}
 }
-	
