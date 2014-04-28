877baba72f00e5beea5e4a2c3b35954ee9c10e83
Matthis Krause <matthiskrause@hotmail.com>
2014-04-28 22:03:32 +0200

    Clean up Main

--- src/studiverwaltung/Main.java
+++ src/studiverwaltung/Main.java
@@ -1,51 +1,9 @@
 package studiverwaltung;
 
-import studiverwaltung.util.structures.HashTable;
-import studiverwaltung.util.structures.BinarySearchTree;
 import studiverwaltung.util.structures.SimpleCollection;
 
 public class Main {
  	public static void main(String[] args) {
-		int[] keys = new int[5];
-		HashTable ht = new HashTable();
-		int k = 31;
-		for (int i = 0; i < keys.length; ++i) {
-			keys[i] = k;
-			k += 31;
-			ht.put(keys[i], i);
-			System.out.printf("key=%d, value=%d%n", keys[i], i);
-			System.out.println(ht + "\n");
-		}
-
-		ht.put(5, 10);
-
-		System.out.println("------------\n");
-
-		for (int i = 0; i < keys.length; ++i) {
-			System.out.printf("key: %d, value: %d%n",
-			    keys[i], ht.get(keys[i]));
-			System.out.println(ht + "\n");
-			ht.remove(keys[i]);
-		}
-
-		System.out.println(ht);
-	}
-
-	private static <T extends Comparable<T>> boolean isValid(
-	    BinarySearchTree<T, T> bst, T[] values)
-	{
-		T max = values[0], min = values[0];
-		for (int i = 0; i < values.length; ++i) {
-			max = max.compareTo(values[i]) > 0 ? max : values[i];
-			min = min.compareTo(values[i]) < 0 ? min : values[i];
-			bst.put(values[i], values[i]);
-		}
-
-		for (int i = 0; i < values.length; ++i) {
-			if (!bst.get(values[i]).equals(values[i])) return false;
-		}
-
-		return bst.getMax().equals(max) && bst.getMin().equals(min);
 	}
 
 	private static void printCollection(SimpleCollection col) {
