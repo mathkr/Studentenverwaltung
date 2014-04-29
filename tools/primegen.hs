8cc89e985d9fdb3a25d8eecdc455e825dddc4554
Matthis Krause <matthiskrause@hotmail.com>
2014-04-29 09:38:37 +0200

    Streamline main function

--- tools/primegen.hs
+++ tools/primegen.hs
@@ -1,7 +1,7 @@
 import Data.Numbers.Primes
 
 main :: IO()
-main = putStr . unlines . map show . getPrimes $ 31
+main = mapM_ print . getPrimes $ 31
 
 getPrimes :: (Integral a) => a -> [a]
 getPrimes max = [smallerPrime x | x <- powersOfTwo max]
