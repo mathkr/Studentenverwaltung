f552204b82897dd35832ce758ccdf3f38adec5c7
Matthis Krause <matthiskrause@hotmail.com>
2014-05-20 10:40:03 +0200

    Start adding print functionality

--- src/polygon/PolygonFrame.java
+++ src/polygon/PolygonFrame.java
@@ -5,12 +5,15 @@ import java.awt.event.*;
 
 public class PolygonFrame extends Frame implements Observer {
 	private RandomPolygon polygon;
+	private PolygonManager polyManager;
 	private Color fgColor;
 
-	public PolygonFrame(RandomPolygon polygon, Color bgColor,
-	    Color fgColor)
+	public PolygonFrame(RandomPolygon polygon, PolygonManager polyManager,
+	    Color bgColor, Color fgColor)
 	{
 		super("Polygon");
+
+		this.polyManager = polyManager;
 
 		setBackground(bgColor);
 		setForeground(fgColor);
@@ -25,10 +28,27 @@ public class PolygonFrame extends Frame implements Observer {
 			}
 		});
 
+		addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				if (e.getKeyChar() == 'p') {
+					polyManager.pauseGenerator();
+					printFrame();
+					polyManager.unpauseGenerator();
+				}
+			}
+
+		});
+
 		this.polygon = polygon;
 		polygon.addObserver(this);
 
 		setVisible(true);
+	}
+
+	public void printFrame() {
+		getToolkit().getPrintJob(this, "Polygon Plotter",
+		    new JobAttributes(), new PageAttributes());
 	}
 
 	public void notifyObserver() {
@@ -36,20 +56,22 @@ public class PolygonFrame extends Frame implements Observer {
 	}
 
 	public void paint(Graphics g) {
-		int[] pointsX = toWindowCoords(polygon.getPointsX(), true);
-		int[] pointsY = toWindowCoords(polygon.getPointsY(), false);
+		int w = getWidth();
+		int h = getHeight();
+
+		int[] pointsX = toWindowCoords(polygon.getPointsX(), w);
+		int[] pointsY = toWindowCoords(polygon.getPointsY(), h);
 		int length = polygon.getSize();
 
 		Polygon p = new Polygon(pointsX, pointsY, length);
 		g.fillPolygon(p);
 	}
 
-	private int[] toWindowCoords(Double[] points, boolean xAxis) {
+	private int[] toWindowCoords(Double[] points, int windowDimension) {
 		int[] res = new int[points.length];
 
 		for (int i = 0; i < points.length; ++i) {
-			res[i] = xAxis ? (int)(getWidth() * points[i]) :
-			    (int)(getHeight() * points[i]);
+			res[i] = (int)(windowDimension * points[i]);
 		}
 
 		return res;
--- src/polygon/PolygonManager.java
+++ src/polygon/PolygonManager.java
@@ -9,21 +9,66 @@ public class PolygonManager {
 
 	private PolygonFrame polygonFrame;
 	private RandomPolygon polygon;
+	private GeneratorThread generatorThread;
+
+	private class GeneratorThread extends Thread {
+			private boolean paused = false;
+			private int sleepTime;
+
+			GeneratorThread(int sleepTime) {
+				this.sleepTime = sleepTime;
+			}
+
+			@Override
+			public void run() {
+				while (true) {
+					while (!paused) {
+						addPoint();
+
+						try {
+							Thread.sleep(sleepTime);
+						} catch (InterruptedException e)
+						{
+							e.printStackTrace();
+						}
+					}
+
+					try {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+
+			public void addPoint() {
+				polygon.addRandomPoints(1);
+			}
+
+			public void pause() {
+				this.paused = true;
+			}
+
+			public void unpause() {
+				this.paused = false;
+			}
+	}
 
 	public PolygonManager() {
 		polygon = new RandomPolygon(3);
-		polygonFrame = new PolygonFrame(polygon, Color.BLACK,
+		polygonFrame = new PolygonFrame(polygon, this, Color.BLACK,
 		    Color.WHITE);
 
-		new Thread(() -> {
-			while(true) {
-				try {
-					Thread.sleep(500);
-					polygon.addRandomPoints(1);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-		}).start();
+		generatorThread = new GeneratorThread(500);
+
+		generatorThread.start();
+	}
+
+	public void pauseGenerator() {
+		generatorThread.pause();
+	}
+
+	public void unpauseGenerator() {
+		generatorThread.unpause();
 	}
 }
